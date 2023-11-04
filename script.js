d5c9b9a37c5d29a1233b4f8e89e54e86c3059777
Dimluar <148585902+Dimluar@users.noreply.github.com>
2023-11-04 23:20:42 +0100

    Display numbers on click

--- script.js
+++ script.js
@@ -27,3 +27,41 @@ function operate(a, b, operator) {
       break;
   }
 }
+
+const delBtn = document.querySelector("#del");
+const acBtn = document.querySelector("#ac");
+const dotBtn = document.querySelector("#dot");
+const equalBtn = document.querySelector("#equal");
+const plusBtn = document.querySelector("#plus");
+const minusBtn = document.querySelector("#minus");
+const multiplyBtn = document.querySelector("#multiply");
+const divisionBtn = document.querySelector("#division");
+
+const numberBtnList = Array.from(document.querySelectorAll(".number-btn"));
+numberBtnList.sort((a, b) => a.textContent - b.textContent);
+console.log(numberBtnList);
+
+const textField = document.querySelector(".text-field");
+let textContentValue = "";
+
+textField.textContent = "0";
+
+document.addEventListener("click", (e) => {
+  switch (e.target) {
+    case acBtn:
+      textField.textContent = "0";
+      textContentValue = "";
+  }
+});
+
+numberBtnList.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    if (
+      (index !== 0 || textField.textContent !== "0") &&
+      textField.textContent.length < 10
+    ) {
+      textContentValue += `${index}`;
+      textField.textContent = textContentValue;
+    }
+  });
+});
